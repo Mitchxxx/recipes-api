a2f08b1bbe81dc8301da51545b5404cb7e0085dc
Mitchxxx <megboko@gmail.com>
2025-07-17 22:44:45 +0100

    Added ListRecipesHandler

--- main.go
+++ main.go
@@ -16,6 +16,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
@@ -26,8 +27,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 var recipes []Recipe
+var err error
+var ctx context.Context
+var client *mongo.Client
+var collection *mongo.Collection
+
 func main () {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
@@ -62,6 +72,20 @@ func NewRecipeHandler(c *gin.Context){
 }
 
 func ListRecipesHandler (c *gin.Context){
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	defer cur.Close(ctx)
+	recipes := make([]Recipe, 0)
+	for cur.Next(ctx) {
+		var recipe Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
 	c.JSON(http.StatusOK, recipes)
 }
 
@@ -130,6 +154,7 @@ func DeleteRecipeHandler (c *gin.Context){
 }
 
 func init(){
+
 	recipes = make([]Recipe, 0)
 
 	file, err := os.Open("recipes.json")
@@ -148,4 +173,28 @@ func init(){
 		log.Printf("Error parsing JSON: %v", err)
 		return
 	}
+  ctx = context.Background()
+  client, err = mongo.Connect(ctx,
+	options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+  if err = client.Ping(context.TODO(),
+	readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+	collection = client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	log.Println("Connected to MongoDB")
+
+	/* code to add recipes to mongo database
+	var listOfRecipes [] interface{}
+	for _, recipe := range recipes {
+		listOfRecipes = append(listOfRecipes, recipe)
+	}
+	collection := client.Database(os.Getenv(
+		"MONGO_DATABASE")).Collection("recipes")
+	insertManyResult, err := collection.InsertMany(ctx, listOfRecipes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Inserted recipes: ", len(insertManyResult.InsertedIDs))
+	*/
 }
